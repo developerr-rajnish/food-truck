35c75e5ba94668626ab07f61a3c94c11c47d62f8
Rajnish Kumar <79629938+RAJNISH648@users.noreply.github.com>
2023-12-22 20:55:53 +0530

    about page add

--- js/script.js
+++ js/script.js
@@ -80,3 +80,22 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 });
+
+// swiper slider js 
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 20,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: { 
+    1200: {
+      slidesPerView: 3,  
+      spaceBetween: 20,
+    },
+  },
+});
+
