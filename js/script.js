4a9654a35e4641b80ce691ab3bbed748484af9b9
Rajnish Kumar <79629938+RAJNISH648@users.noreply.github.com>
2023-12-27 01:24:16 +0530

    nav update

--- js/script.js
+++ js/script.js
@@ -16,53 +16,6 @@ function closeMenu() {
   navMenu.classList.remove("active");
 }
 
-// nav add remove class
-let nav1 = document.querySelector(".nav1");
-let nav2 = document.querySelector(".nav2");
-let nav3 = document.querySelector(".nav3");
-let nav4 = document.querySelector(".nav4");
-let nav5 = document.querySelector(".nav5");
-
-function navLink1() {
-  nav1.classList.add("active");
-  nav2.classList.remove("active");
-  nav3.classList.remove("active");
-  nav4.classList.remove("active");
-  nav5.classList.remove("active");
-}
-
-function navLink2() {
-  nav1.classList.remove("active");
-  nav2.classList.add("active");
-  nav3.classList.remove("active");
-  nav4.classList.remove("active");
-  nav5.classList.remove("active");
-}
-
-function navLink3() {
-  nav1.classList.remove("active");
-  nav2.classList.remove("active");
-  nav3.classList.add("active");
-  nav4.classList.remove("active");
-  nav5.classList.remove("active");
-}
-
-function navLink4() {
-  nav1.classList.remove("active");
-  nav2.classList.remove("active");
-  nav3.classList.remove("active");
-  nav4.classList.add("active");
-  nav5.classList.remove("active");
-}
-
-function navLink5() {
-  nav1.classList.remove("active");
-  nav2.classList.remove("active");
-  nav3.classList.remove("active");
-  nav4.classList.remove("active");
-  nav5.classList.add("active");
-}
-
 // scroll page add class
 document.addEventListener("DOMContentLoaded", function () {
   window.addEventListener("scroll", function () {
